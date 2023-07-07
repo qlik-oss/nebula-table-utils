9bbec60be530005380094d18a7c5e6686ec91bd5
a-m-dev <a.mirzaei.dev@gmail.com>
2023-07-07 15:35:21 +0200

    feat: build declaration files

--- src/components/ComponentTwo.tsx
+++ src/components/ComponentTwo.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import { Shared } from './Shared';
 
-const ComponentTwo = () => {
+export interface IComp2Props {
+  valOne: string;
+  valTwo: boolean;
+}
+
+const ComponentTwo = (args: IComp2Props) => {
   const [state, setState] = useState(10);
   const handleClick = () => setState(state + 10);
   return (
