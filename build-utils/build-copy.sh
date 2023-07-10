b1aa7ac89a47568b7f3db4319a861c28bc8cbc07
a-m-dev <a.mirzaei.dev@gmail.com>
2023-07-10 10:56:11 +0200

    update bash script to support sn-table as well

--- build-utils/build-copy.sh
+++ build-utils/build-copy.sh
@@ -6,8 +6,40 @@ yarn build:prod
 
 export $(grep -v '^#' .env.local | xargs)
 
-PKG_PLACEMENT_DIR="@qlik-oss/nebula-table-utils";
+PKG_PLACEMENT_DIR="node_modules/@qlik-oss/nebula-table-utils";
 
-rm -rf "${PVT_NODE_MODULES_DIR}/${PKG_PLACEMENT_DIR}";
-mkdir "${PVT_NODE_MODULES_DIR}/${PKG_PLACEMENT_DIR}";
-cp -r ./dist/ "${PVT_NODE_MODULES_DIR}/${PKG_PLACEMENT_DIR}";
+BOLD="\033[1m";
+NO_BOLD="\033[0m";
+GREEN="\033[0;32m";
+NO_COLOR="\033[0m";
+
+
+handleCopy() {
+  local NAME=${1};
+  local DIR=${2};
+
+  if [ -n "$DIR" ]; 
+  then
+    echo "=========================================================";
+    echo "${BOLD}${NAME}${NO_BOLD}";
+    echo "- ${NAME} variable provided (check)";
+
+    PKG_PATH="${DIR}/${PKG_PLACEMENT_DIR}";
+
+    if [ -d "$PKG_PATH" ];
+    then 
+      echo "- pkg exists -> try to remove and replace! (check)";
+      rm -rf $PKG_PATH;
+    fi
+    
+    mkdir -p $PKG_PATH;
+    cp -r ./dist/ $PKG_PATH;
+
+    
+    echo "${GREEN}${BOLD}✅ UPDATED ${NAME} WITH LATES CHANGES IN 'nebula-table-utils'${NO_BOLD}${NO_COLOR}";
+    echo "=========================================================";
+  fi
+}
+
+handleCopy "PVT" $PV_TABLE_DIR;
+handleCopy "SNT" $SN_TABLE_DIR;
