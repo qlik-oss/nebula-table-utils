b6c920c441a079fbc5abe5ad98861736de535615
a-m-dev <a.mirzaei.dev@gmail.com>
2023-07-13 13:12:36 +0200

    chore: upd devbuild flow

--- .github/scripts/dev-build.sh
+++ .github/scripts/dev-build.sh
@@ -12,12 +12,6 @@ git fetch --depth=10000
 # the actual commit
 git_rev=$(git describe --tags --always --abbrev=7 ${_sha} --match "v[0-9]*.[0-9]*.[0-9]*")
 
-echo "++++++++++++++++";
-git describe --tags --always
-git describe --tags --always --abbrev=7
-git describe --tags --always --abbrev=0 --match "v[0-9]*.[0-9]*.[0-9]*"
-echo "++++++++++++++++";
-
 echo "===============";
 echo $SHA;
 echo $git_rev;
@@ -26,7 +20,7 @@ echo "===============";
 # If git revision is not an exact semver tag, then bump patch
 # An exact semver does not contain a '-'
 if [[ "$git_rev" == *-* ]]; then
-  echo 1;
+  echo "TRANSFORMING VERSION STRING"
   # Transforms 0.0.0-0-g1234abc to 0.0.1-0.g123abc
   git_rev=$(echo $git_rev | perl -ne 'm/(^v\d+\.\d+\.)(\d+)(.*)(\-g)(.*$)/ && print $1 . int(1+$2) . $3 . ".g" . $5')
 fi
@@ -38,14 +32,12 @@ fi
 VER=${git_rev#v}
 # On tag push that matches refs/tags/v*.*.*, use that version regardless of git describe
 if echo "$GITHUB_REF" | grep -E 'refs/tags/v[0-9]+\.[0-9]+\.[0-9]+$'; then
-  echo 2;
   VER=${GITHUB_REF#*/v}
   RELEASE_TAG="1"
 fi
 
-echo "Set version: ${VER}"
 if (echo $VER | egrep -- '-[0-9a-z.]+$' 1> /dev/null); then
-  echo 3;
+  echo "UPDATING VERSION in pkg.json for dev build to: ${VER}"
   npm version $VER --no-git-tag-version
 fi
 
