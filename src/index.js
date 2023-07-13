f1e3b8d1a4af889a2e2a804f318b2fcda40c98c8
a-m-dev <a.mirzaei.dev@gmail.com>
2023-07-13 11:40:50 +0200

    chore: upd

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log('You can use this pkg as it is, import what ever component you want seperately!');
+// eslint-disable-next-line import/prefer-default-export
 export { default as ReactFromModule } from 'react';
