ea2e4b4121765bb53be8abe61d1985ea076ca677
a-m-dev <a.mirzaei.dev@gmail.com>
2023-07-17 12:58:40 +0200

    chore: upd

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
-export default {
+module.exports = {
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
   testRegex: 'src/.+\\.(test|spec)\\.[jt]sx?$',
   transform: {
-    '^.+\\.[jt]sx?$': '@swc/jest',
+    '^.+\\.(t|j)sx?$': '@swc/jest',
   },
   coveragePathIgnorePatterns: ['dist', 'lib', 'build-utils'],
 };
