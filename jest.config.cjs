bf91f8a43fe12f5918ee32901ae573c41dd2bcc0
a-m-dev <a.mirzaei.dev@gmail.com>
2023-07-17 15:10:34 +0200

    chore: upd

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,6 @@
 module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
   testRegex: 'src/.+\\.(test|spec)\\.[jt]sx?$',
   transform: {
