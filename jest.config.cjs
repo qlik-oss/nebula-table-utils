6d06dbfd2bb5d47109326dae0d52430d4e40fedb
Christofer Bengtsson <cbt@qlik.com>
2023-09-14 09:00:00 +0200

    chore: update jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,13 +5,12 @@ module.exports = {
   testEnvironment: 'jsdom',
   testRegex: 'src/.+\\.(test|spec)\\.[jt]sx?$',
   transform: {
-    // '\\.js$': 'babel-jest',
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
   moduleNameMapper: {
     '@qlik-trial/sprout/icons/.*': '<rootDir>/test-utils/sprout-icons-mock.tsx',
   },
   coveragePathIgnorePatterns: ['dist', 'lib', 'build-utils', './src/constants'],
-  transformIgnorePatterns: ['node_modules/.pnpm/(?!(cliui|wrap-ansi|d3-color)@)'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/.pnpm/(?!(cliui|wrap-ansi|d3-color)@)'],
   setupFilesAfterEnv: ['<rootDir>/jest/setup.js'],
 };
