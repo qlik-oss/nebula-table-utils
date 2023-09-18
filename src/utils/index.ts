e966588495175497fab5515a4aa71782d4582598
Christofer Bengtsson <cbt@qlik.com>
2023-09-18 13:59:15 +0200

    feat: paginiation footer utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,5 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as getHoverColor } from './get-hover-color';
+export { default as COLORING } from './coloring';
+export { default as focusSelectionToolbar } from './focus-selection-toolbar';
+export { default as preventDefaultBehavior } from './prevent-default-behavior';
+export { default as handleLastTab } from './handle-last-tab';
