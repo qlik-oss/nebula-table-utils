c47c7b70caf3ac6c10adf3deade3d3f6f9781af9
Christofer Bengtsson <cbt@qlik.com>
2023-09-18 10:33:44 +0200

    feat: use extended theme

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as getHoverColor } from './get-hover-color';
+export { default as isTransparentColor } from './is-transparent-color';
+export { default as isDarkColor } from './is-dark-color';
