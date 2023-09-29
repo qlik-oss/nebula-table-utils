33ed1df5b886d28d2e06c4e18bc91ca2bf935254
Christofer Splittorff <cbt@qlik.com>
2023-09-29 11:07:45 +0200

    feat: use measure text (#65)
    * feat: use measure text
    
    * fix: support font style
    
    * fix: export types
    
    * fix: review comment
    
    Co-authored-by: Ahmad Mirzaei <a.mirzaei.dev@gmail.com>
    
    * fix: move chart modules to peer dep
    
    ---------
    
    Co-authored-by: Ahmad Mirzaei <a.mirzaei.dev@gmail.com>

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,3 +4,9 @@ export { default as useFetch } from './use-fetch';
 export { default as useWaitForFonts } from './use-wait-for-fonts';
 export { default as useReactRoot } from './use-react-root';
 export { default as useExtendedTheme } from './use-extended-theme';
+export {
+  default as useMeasureText,
+  type UseMeasureTextProps,
+  type MeasureTextHook,
+  type EstimateLineCount,
+} from './use-measure-text';
