f4a130fbaf0abcdc4d2f8f5d66a6d9e7249349b3
Niek van Staveren <niek.vanstaveren@qlik.com>
2024-01-23 14:51:41 +0100

    chore: prepare monorepo

--- .github/scripts/create-npmrc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
-
-echo "//npm.pkg.github.com/:_authToken=$GH_TOKEN" >> ~/.npmrc
-echo "//npm.pkg.github.com/:always-auth=true" >> ~/.npmrc
-
-cat ~/.npmrc
